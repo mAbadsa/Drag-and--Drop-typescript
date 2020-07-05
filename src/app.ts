b8aec73bafe787df9290a94651045491e38fd02d
Mu'hammad Alabadsa <42316204+mAbadsa@users.noreply.github.com>
2020-07-05 17:05:21 +0300

    Rendering Projects List To The DOM

--- src/app.ts
+++ src/app.ts
@@ -61,6 +61,43 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
+// Project List Class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importElement = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importElement.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-project-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
+// Project Input Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -155,3 +192,5 @@ class ProjectInput {
 }
 
 const projInput = new ProjectInput();
+const activeProjectList = new ProjectList("active");
+const finishProjectList = new ProjectList("finished");
